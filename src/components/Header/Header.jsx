c2603226b9594234aaabd7901b2dbf88b37cabc4
Sanket <msanket450@gmail.com>
2022-11-11 11:40:08 +0530

    last commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,8 +14,10 @@ function Header({ off, img, products, setProducts }) {
           <div>
             <h1>{off}% OFF</h1>
             <p>On all baby care products sourced from trusted network of verified pharmacies and medical stores </p>
-            <button>Buy now</button>
-            <span>&gt;</span>
+            <span className="buttons">
+              <button>Buy now</button>
+              <span>&gt;</span>
+            </span>
           </div>
         </div>
         <div className='app__header-image'>
